d61cb0a9c339fcfd32a406c0db60e5601a2c6d09
Oleg-Intaro <peteshenkov@intaro.ru>
2014-09-12 16:53:55 +0400

    Сгенерировал сущности и связи между ними

---
+++ src/Intaro/BookBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Intaro\BookBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('IntaroBookBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
